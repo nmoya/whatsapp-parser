73c29cb53a93d9452301074af5f6a058ffac56cf
nmoya <nikolasmoya@gmail.com>
2014-03-08 00:55:03 +0000

    Working on a php server via file upload

---
+++ index.php
@@ -0,0 +1,6 @@
+
+<form action="upload_file.php" method="post" enctype="multipart/form-data">
+<label for="file">Whatsapp text file:</label>
+<input type="file" name="file" id="file"><br>
+<input type="submit" name="submit" value="Submit">
+</form>
