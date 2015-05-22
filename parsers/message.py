24901cf82afd7a72463a17d5964555229e1db494
nmoya <nikolasmoya@gmail.com>
2015-05-22 02:36:15 -0300

    Addind repr method in message.py

--- parsers/message.py
+++ parsers/message.py
@@ -6,3 +6,6 @@ class Message():
 		self.date                  = date
 		self.time                  = time
 		self.datetime_obj          = datetime_obj
+
+	def __repr__(self):
+		return " ".join([self.datetime_obj, self.sender, self.content])
