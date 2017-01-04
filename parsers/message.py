12b54254ee78209e47e13497ec0547e527eec568
Manvendra Singh <manvendra0310@gmail.com>
2017-01-04 23:01:31 +0530

    fixes type error in join function expects list of strings

--- parsers/message.py
+++ parsers/message.py
@@ -8,4 +8,4 @@ class Message():
         self.datetime_obj          = datetime_obj
 
     def __repr__(self):
-        return " ".join([self.datetime_obj, self.sender, self.content])
+        return " ".join(str(v) for v in [self.datetime_obj, self.sender, self.content])
