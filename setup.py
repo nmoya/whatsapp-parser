bc209fa342ea8f65bdbaf46ef1de8c5ec862100a
Nikolas Moya <moya@work.co>
2017-02-02 18:02:27 +0100

    Add pip config files

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+try:
+    import pypandoc
+    import codecs
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    long_description = codecs.open('README.rst', 'w', 'utf-8').read()
+
+version = '1.0.0'
+
+setup(
+    name='whatsapp parser',
+    version=version,
+    author='Nikolas Moya',
+    author_email='nikolasmoya@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://github.com/nmoya/whatsapp-parser',
+    license='MIT',
+    description='',
+    long_description=long_description,
+    entry_points={
+        'console_scripts': [
+            'wp_parser=wp_parser.chat:main',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7 :: Only',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
